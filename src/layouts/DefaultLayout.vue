eff095142df8826192a0199ab50ae9e68da6f06d
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-04-25 14:06:51 +0300

    demo layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import router from '@/router';
+
+router.getRoutes()
+</script>
+
+<template>
+<div>
+    <ul>
+      <li v-for="route in router.getRoutes()" :key="route.name">
+        <RouterLink :to="route.path" class="capitalize"> {{ route.name }} </RouterLink>
+      </li>
+    </ul>
+    <main class="bg-red">
+        <slot></slot>
+    </main>
+</div>
+</template>
+
+<style scoped>
+.bg-red {
+    background-color: red;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import router from '@/router';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
 </script>
 
 <template>
-  <main>
+  <main>   
     <TheWelcome />
+
+    <DefaultLayout>
+      qweqwewqqwwqe
+    </DefaultLayout>
   </main>
 </template>
+
+<style scoped>
+.capitalize {
+  text-transform: capitalize;
+}
+</style>
