e5a285ff520b22dbe8d0521faf6c120a509177fa
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-04-28 16:48:32 +0300

    Contact page

--- src/pages/contacts.vue
+++ src/pages/contacts.vue
@@ -1,8 +1,13 @@
 <template>
-  <input type="text" placeholder="Type your name">
+  <input type="text" ml-10 mr-3 p-1 placeholder="Type your name">
+  <input type="text" p-1 mr-3 placeholder="Type your email">
+  <input type="tel" p-1 mr-3 placeholder="Type your phone number">
+  <button px-5 py-4 rounded-2 transition-colors-300 bg-gray ml-2 text-white hover:bg-slate-3>
+    Send
+  </button>
 </template>
 
 <route lang="yaml">
 meta:
   showOnNavbar: true
-</route>
+  </route>
