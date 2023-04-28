578abe6cecbb8e2f688fc3b3f8f70268a3ebcf43
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-04-28 11:53:01 +0300

    Create demo ecommerce page

---
+++ src/views/ECommerceView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <article>
+      <img src="https://via.placeholder.com/300x200.png?text=Product+1" alt="Product 1">
+      <h3>Product 1</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, lectus eget convallis congue, sem
+        purus accumsan tortor, eget malesuada arcu elit non elit. Nulla vel malesuada massa.
+      </p>
+      <p><strong>BGN19.99</strong></p>
+      <button>Add to Cart</button>
+    </article>
+
+    <article>
+      <img src="https://via.placeholder.com/300x200.png?text=Product+2" alt="Product 2">
+      <h3>Product 2</h3>
+      <p>
+        Phasellus eget mi lacus. Maecenas sed nibh tortor. Integer consequat lacus nec metus dignissim, ut mollis
+        nisl auctor. Nullam vel augue id velit dictum semper.
+      </p>
+    </article>
+  </section>
+</template>
