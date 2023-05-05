4c680941fb8f894c7e1fcf15dbbb6fa3853684be
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-05 13:54:31 +0300

    seting up the design of about cards

---
+++ src/components/AboutCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description?: string
+  image: string
+  link: {
+    label: string
+    redirect: string
+  }
+}>()
+</script>
+
+<template>
+  <div
+    b="4 slate" p-4 sm:p-8 max-w-75 w-full
+    rounded-3 overflow-hidden bg-slate-6
+    space-y-6
+  >
+    <h4 class="font-inter font-extrabold text-(2xl center) leading-relaxed">
+      {{ title }}
+    </h4>
+    <div flex="~ col" items-center gap-3>
+      <div w-50 h-40>
+        <img :src="image" object-contain mx-auto h-full>
+      </div>
+      <a
+        inline-block mx-auto font-medium text-sm
+        :href="link.redirect" target="_blank"
+      >
+        {{ link.label }}</a>
+    </div>
+    <p v-if="description" class="mt10px leading-7 text-white text-center">
+      {{ description }}
+    </p>
+  </div>
+</template>
