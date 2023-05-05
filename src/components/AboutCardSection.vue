025b2d5c4776370b31115d04196cf0509d719bf6
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-05 13:55:12 +0300

    setting up about section

---
+++ src/components/AboutCardSection.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AboutCard from '@/components/AboutCard.vue'
+
+defineProps<{
+  cards: AccountCard[]
+}>()
+</script>
+
+<template>
+  <div class="mx-auto  max-w-300 p-4 grid gap-6 lg:grid-cols-4 lt-lg:w-fit justify-items-center">
+    <AboutCard
+      v-for="card in cards" :key="card.title"
+      v-bind="card"
+    />
+  </div>
+</template>
