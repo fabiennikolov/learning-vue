be3c73b40ff7312d6d0f9d195c6035f728a8d83f
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-05 13:56:09 +0300

    Importing the aboutcards and its properties

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,28 +1,82 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import AboutCardSection from '@/components/AboutCardSection.vue'
+
+const cards = ref<AccountCard[]>([
+  {
+    title: 'Vue3 Enthusiast',
+    link: {
+      label: 'Practising Vue3',
+      redirect: 'https://vuejs.org/',
+    },
+    image: '/src/components/icons/vue.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt sem eget ultricies eleifend. Nunc et dolor sed tellus luctus molestie. Fusce dapibus consequat risus ut fringilla.',
+    class: 'text-lime',
+  },
+  {
+    title: 'TypeScript lover',
+    link: {
+      label: 'Learning new stuff in TS',
+      redirect: 'https://www.typescriptlang.org/',
+    },
+    image: 'src/components/icons/Typescript_logo_2020.svg.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt sem eget ultricies eleifend. Nunc et dolor sed tellus luctus molestie. Fusce dapibus consequat risus ut fringilla.',
+    class: 'text-blue',
+  },
+  {
+    title: 'HTML architect',
+    link: {
+      label: 'Visit my GitHub account',
+      redirect: 'https://github.com/fabiennikolov',
+    },
+    image: 'src/components/icons/174854.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt sem eget ultricies eleifend. Nunc et dolor sed tellus luctus molestie. Fusce dapibus consequat risus ut fringilla.',
+    class: 'text-orange-6',
+  },
+  {
+    title: 'CSS enjoyer',
+    link: {
+      label: 'Learning new stuff in CSS',
+      redirect: 'https://unocss.dev/',
+    },
+    image: 'src/components/icons/csslogo1.png',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt sem eget ultricies eleifend. Nunc et dolor sed tellus luctus molestie. Fusce dapibus consequat risus ut fringilla.',
+    class: 'text-blue',
+  },
+])
+</script>
+
 <template>
   <h2 style="text-align:center" mt25px text-4xl>
     About Me
   </h2>
-  <div class="container space-y-6 p-4 m-5 grid-cols-3">
-    <div class="ml15 mt15 w300px">
-      <div class="border-4 p-4 sm:p-8">
-        <div class="font-inter font-extrabold text-2xl text-black tracking-tight pl18px">
-          VueJs Enthusiast
-        </div>
-        <div class="mt-1 font-medium text-sm text-lime">
-          <div class="img">
-            <img src="/src/components/icons/vue.png" alt="">
-          </div>
-          <a href="https://github.com/fabiennikolov/learning-vue" target="_blank">Learning web-dev</a>
-        </div>
-        <p class="mt-4 leading-7 text-slate-500">
-          Learning new stuffs in VueJs
-        </p>
-        <p class="mt-4 leading-7 text-slate-500">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        </p>
-      </div>
-    </div>
-  </div>
+  <AboutCardSection :cards="cards" />
+  <!-- <div class="mx-auto p-4 grid gap-4 lg:grid-cols-3 lt-lg:w-fit justify-items-center">
+    <AboutCard
+
+      text-lime
+    />
+    <AboutCard
+      title="HTML learner"
+      :link="{
+        label: 'Visit my GitHub account',
+        redirect: 'https://github.com/fabiennikolov/learning-vue',
+      }"
+      image="/src/components/icons/174854.png"
+      description="Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      text-orange-6
+    />
+    <AboutCard
+      title="CSS enjoyer"
+      :link="{
+        label: 'Learning unocss',
+        redirect: 'https://unocss.dev/',
+      }"
+      image="/src/components/icons/csslogo1.png"
+      description="Learning new stuffs in CSS. Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+      text-blue
+    />
+  </div> -->
 </template>
 
 <route lang="yaml">
