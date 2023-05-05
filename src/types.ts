9a738a4fa02e0cfdae2a78b25add315e4c6e39fb
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-05 13:53:36 +0300

    Adding Types for aboutcards

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,20 @@ declare global {
     image: string
     price: number
   }
+
+  interface Link {
+    label: string
+    redirect: string
+  }
+
+  type Links = Link[]
+  interface AccountCard {
+    title: string
+    description?: string
+    image: string
+    link: Link
+    class?: string
+  }
 }
 
 export {}
