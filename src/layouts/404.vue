f72fa727ddaa6c55303181d72d5f16446651fcd9
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-10 14:56:20 +0300

    setting up the 404 page path

---
+++ src/layouts/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div text-center b-black b-2 class="bg-#8abcbc">
+    <div class="text-220px display-inline-block h450px tracking-15px">
+      <h1>404</h1>
+      <hr class="p0 b-t-5px text-center w400px h10px">
+    </div>
+    <div class="uppercase text-center tracking-12px text-4em">
+      <p>the page</p>
+    </div>
+    <div class="uppercase text-45px text-center mt19px w100% leading-1.2em">
+      <p>was not found</p>
+    </div>
+    <RouterLink to="/home" text-25px uppercase bg-white position-relative display-inline-block w358px p5px mb10px mt10px rounded-3 b-black b-2 hover:bg-slate-3>
+      Go to homepage
+    </RouterLink>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,15 @@ import routes from '~pages'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+
+  // routes,
+  // [...routes,
+  //   { path: '/404', component: NotFound },
+  //   { path: '*', redirect: '/404' }
+  // ],
 })
+
+router.addRoute({ path: '/404', name: 'NotFound', component: Error })
+router.addRoute({ path: '/:pathMatch(.*)*', redirect: '/404' })
 
 export default router
