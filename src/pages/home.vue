15217548f3b7baa626f77c553c9d0fcd3d668fb5
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-16 15:17:20 +0300

    dynamic title, description and msg

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('Hi, I am Fab')
+const description = ref('I am learning web development using Vue3. In my spare time I enjoy playing chess and building my own projects.')
+const msg = ref('Here in this website im creating my first responsive website layout for different type of pages')
 </script>
 
 <template>
@@ -8,14 +13,14 @@
     space-y-6 ma mb-3 rounded-3 b-3 b-slate-3"
   >
     <h1 text-40px my-2 ml3 text-5xl>
-      Hi, I'm Fab
+      {{ title }}
     </h1>
     <div text-xl grid gap-4 ml3 leading-10 text-5 tracking-wider>
       <p mt1rem>
-        I'm learning web development using Vue3. In my spare time I enjoy playing chess and building my own projects.
+        {{ description }}
         <br> I'm on <a class="hover:text-red5 hover:no-underline text-blue-7" href="https://github.com/fabiennikolov" target="_blank">GitHub</a>
         <a class="hover:text-red5 hover:no-underline text-blue-7" href="https://www.instagram.com/fabch0/" target="_blank"> instagram</a><br>
-        Here in this website im creating my first responsive website layout for different type of pages
+        {{ msg }}
       </p>
     </div>
   </div>
