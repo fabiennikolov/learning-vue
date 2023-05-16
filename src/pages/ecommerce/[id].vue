44758d771617c89bdd659e51c080b40b89b911d1
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-16 15:17:36 +0300

    dynamic description

--- src/pages/ecommerce/[id].vue
+++ src/pages/ecommerce/[id].vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
+
+const description = ref('Lorem ipsum asdajlwijalwijfalisjf')
 
 const route = useRoute()
 const id = computed(() => route.params.id)
@@ -11,8 +13,7 @@ const id = computed(() => route.params.id)
     <img :src="`https://via.placeholder.com/300x200.png?text=Product+${id}`" alt="Product 1">
     <h3>Product {{ id }}</h3>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, lectus eget convallis congue, sem
-      purus accumsan tortor, eget malesuada arcu elit non elit. Nulla vel malesuada massa.
+      {{ description }}
     </p>
     <button>Add to Cart</button>
   </article>
