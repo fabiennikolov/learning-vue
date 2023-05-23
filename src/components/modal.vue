af4dd3e9eb093cfc7bf4326d8cec372a7919b6ae
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-23 14:20:43 +0300

    adding modal

---
+++ src/components/modal.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const visible = ref(false)
+</script>
+
+<template>
+  <div class="card flex justify-center">
+    <Button label="Send" @click="visible = true" />
+    <Dialog
+      v-model:visible="visible"
+      modal
+      header="Header"
+      :style="{ width: '50vw' }"
+      :breakpoints="{ '960px': '75vw', '641px': '100vw' }"
+    >
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </Dialog>
+  </div>
+</template>
