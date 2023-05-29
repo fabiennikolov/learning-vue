d23945a5c56200904cd109c04f3549608c0c7a51
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-29 13:36:04 +0300

    title for product id

--- src/pages/ecommerce/[id].vue
+++ src/pages/ecommerce/[id].vue
@@ -9,6 +9,9 @@ const id = computed(() => route.params.id)
 </script>
 
 <template>
+  <h1 class="text-center mb3 text-4xl ">
+    Here is some additional info about Product {{ id }}
+  </h1>
   <article class="max-w-75 ml15">
     <img :src="`https://via.placeholder.com/300x200.png?text=Product+${id}`" alt="Product 1">
     <h3 class="mt3 text-center">
@@ -17,7 +20,7 @@ const id = computed(() => route.params.id)
     <p>
       {{ description }}
     </p>
-    <button class=" px-3 py-2 rounded-2 transition-colors-300 shadow-[0px_4px_4px_#323232] b-1px hover:bg-slate-2 mt3">
+    <button class="px-3 py-2 rounded-2 transition-colors-300 shadow-[0px_4px_4px_#323232] b-1px hover:bg-slate-2 mt3">
       Add to Cart
     </button>
   </article>
