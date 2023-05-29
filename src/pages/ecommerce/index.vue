f1daf90064d39f8097466079fd5347dc1552a929
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-29 13:35:50 +0300

    title for eccommerce

--- src/pages/ecommerce/index.vue
+++ src/pages/ecommerce/index.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 import Card from '@/components/Card.vue'
 
+const title = ref('This is a sample ecommerce page')
+
 const products = ref<ProductCardData[]>([
   { name: 'Product 1', image: 'Product+1', price: 13.99, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque risus aliquet sagittis iaculis.' },
   { name: 'Product 2', image: 'Product+2', price: 20.99, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque risus aliquet sagittis iaculis.' },
@@ -11,6 +13,9 @@ const products = ref<ProductCardData[]>([
 </script>
 
 <template>
+  <h1 class="text-center text-4xl my5">
+    {{ title }}
+  </h1>
   <section flex items-center gap-4 flex-wrap justify-center text-center>
     <Card
       v-for="product in products" :key="product.name"
