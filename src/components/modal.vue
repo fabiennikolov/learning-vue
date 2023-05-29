cd63c1dc3381ed4a3e09cece6c72e56db9ad41bc
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-05-29 12:58:53 +0300

    fixing modal

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -5,14 +5,9 @@
 <template>
   <div class="flex justify-center b-2">
     <span class="close">&times;</span>
+    <h1 class="text-2xl font-bold mb-8">Thank you for your message</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-      mollit anim id est laborum.
+      We will contact you in 24h
     </p>
   </div>
 </template>
