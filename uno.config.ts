3665775743cacf14184ffc8d07e7aa3851cecb17
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-05 17:04:42 +0300

    creating style component

--- uno.config.ts
+++ uno.config.ts
@@ -5,6 +5,9 @@ import {
 } from 'unocss'
 
 export default defineConfig({
+  shortcuts: [
+    { 'form-link': 'rounded-3 py-1 px-2 hover:bg-slate-5 b-white b-2 transition-colors-300' },
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
