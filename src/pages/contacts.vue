daca9cf3e58607824bb29050c5e33e2a0191ec91
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-05 17:03:23 +0300

    styling contacts and form input setup

--- src/pages/contacts.vue
+++ src/pages/contacts.vue
@@ -3,82 +3,75 @@ const showModal = ref(false)
 
 const title = ref('Contacts')
 const msg = ref('Let me know if you have any questions.')
+
+const form = reactive({
+  name: '',
+  email: '',
+  number: '',
+  comment: '',
+})
+
+const modal = ref<HTMLElement | null>(null)
+
+onClickOutside(modal, () => {
+  showModal.value = false
+})
+
+const loading = ref(false)
+async function submit() {
+  loading.value = true
+  const res = await axios.post('https://jsonplaceholder.typicode.com/posts', form)
+  loading.value = false
+}
 </script>
 
 <template>
-  <div class="home">
-    <button id="show-modal" @click="showModal = true">
-      Show Modal
-    </button>
-
-    <Teleport to="body">
-      <Modal v-model:show="showModal" />
-    </Teleport>
-  </div>
-  <h2 text-center my5 text-5xl class="">
+  <h2 text-center my5 text-5xl>
     {{ title }}
   </h2>
+
   <p text-center my1 text-2xl>
     {{ msg }}
   </p>
-  <div px-2 md:px-5 lg:px-6>
-    <form class="border-[#B2BDD4] bg-gray-8 max-w-200 text-white p-5 rounded-2 my-2rem mxa">
-      <input
-        id="Name" placeholder="Full name" class="px-5 text-white my-1 py-1 bg-transparent
-      border-none focus:ring-none focus-visible:outline-none w-full truncate"
-      >
-      <hr>
-      <input
-        id="Email" placeholder="Email" class="px-5 text-white my-1 my-1 py-1 bg-transparent
-      border-none focus:ring-none focus-visible:outline-none w-full truncate"
-      >
-      <hr>
-      <input
-        id="number" placeholder="Number" class="px-5 my-1 py-1 border-[#B2BDD4] rounded-lg
-       text-white py-1 p-none bg-transparent focus:ring-none focus-visible:outline-none w-full
-       truncate"
-      >
-      <hr>
-      <div>
-        <textarea
-          id="comment" class="my-20px h-40 py-2.5 px-5 my-1 w-full flex items-center gap-2 border-1
-           border-[#B2BDD4] rounded-lg text-white
-         shadow-none bg-transparent transition-box-shadow duration-300 focus:ring-transparent
-          focus-visible:outline-none focus:shadow-neutral-3/50
-          focus:border-neutral-4 focus:shadow-[0_0_0_0.2rem_var(--un-shadow-color)] resize-none
-          placeholder:leading-5.5" placeholder="Comment..." maxlength="5000"
-        />
-      </div>
-      <button
-        class="hover:bg-slate-5 mt10 rounded-full text-black px-5 py-4 w-full relative transition-colors-250
-       bg-gray-7 text-white font-500 overflow-hidden hover:after:opacity-65 after:z-0 after:block
-        after:absolute after:absolute-stretch after:transition-all-250
-        after:bg-image-[var(--dark-gradient)]" type="submit"
-      >
-        <div class="w-full h-full relative z-1">
-          <span class="visible">Send</span><div
-            class="hidden absolute absolute-stretch items-center
-          justify-center"
-          />
+
+  <form @submit.prevent="submit">
+    <div px-2 md:px-5 lg:px-6>
+      <div class="border-[#B2BDD4] bg-gray-8 max-w-200 text-white p-5 rounded-2 my-2rem mxa">
+        <FormInput id="Name" v-model="form.name" placeholder="Full name" />
+        <FormInput id="Email" v-model="form.email" type="email" placeholder="Email" />
+        <FormInput id="Number" v-model="form.number" placeholder="Number" />
+        <FormTextarea id="comment" v-model="form.comment" placeholder="Comment..." />
+
+        <button
+          id="show-modal" class="hover:bg-slate-5 mt10 rounded-full text-black px-5 py-4 w-full relative
+        transition-colors-250 bg-gray-7 text-white font-500 hover:after:opacity-65
+        after:z-0 after:absolute after:transition-all-250 text-center" type="submit"
+          @click="showModal = true"
+        >
+          Send
+        </button>
+        <div class="flex gap-4 mt3 place-content-evenly">
+          <a
+            class="form-link"
+            href="https://github.com/fabiennikolov" target="_blank"
+          >GitHub</a>
+          <a
+            class="form-link"
+            href="https://www.instagram.com/fabch0/" target="_blank"
+          > instagram</a>
+          <a href="tel:0897474851" class="form-link">
+            <i>0897474851</i></a>
         </div>
-      </button>
-      <div class="flex gap-4 mt3 place-content-evenly">
-        <a
-          class="rounded-3 py.5 px.6 hover:bg-slate-5 bg-slate6 b-white b-2"
-          href="https://github.com/fabiennikolov" target="_blank"
-        >GitHub</a>
-        <a
-          class="rounded-3 py.5 px.6 hover:bg-slate-5 bg-slate6 b-white b-2"
-          href="https://www.instagram.com/fabch0/" target="_blank"
-        > instagram</a>
-        <a href="tel:0897474851" class="rounded-3 b-2 py.5 px.6 hover:bg-slate-5">
-          <i>0897474851</i></a>
       </div>
-    </form>
-  </div>
+    </div>
+  </form>
+
+  <Teleport ref="modal" to="body">
+    <Modal v-model:show="showModal" />
+  </Teleport>
 </template>
 
 <route lang="yaml">
 meta:
   showOnNavbar: true
-  </route>
+</route>
