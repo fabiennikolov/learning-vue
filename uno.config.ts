0c066e9b5aa201169d6a842244bb618f3bb955f5
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-06 17:44:58 +0300

    styling home-link

--- uno.config.ts
+++ uno.config.ts
@@ -7,7 +7,7 @@ import {
 export default defineConfig({
   shortcuts: [
     { 'form-link': 'rounded-3 py-1 px-2 hover:bg-slate-5 b-white b-2 transition-colors-300' },
-    { 'home-link': 'text-25px uppercase bg-white display-inline-block p5px mb10px mt10px rounded-3 b-black b-2 hover:bg-slate-3 text-center' },
+    { 'home-link': 'text-7.5 uppercase bg-white display-inline-block p-2 my-4 rounded-3 b-black b-2 hover:bg-slate-3 text-center' },
   ],
   presets: [
     presetUno(),
