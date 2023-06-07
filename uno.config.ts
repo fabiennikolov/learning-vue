75eb31f3f392fc7601124ba369847931a25e7c1d
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-07 15:07:14 +0300

    creating css shortcut

--- uno.config.ts
+++ uno.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   shortcuts: [
     { 'form-link': 'rounded-3 py-1 px-2 hover:bg-slate-5 b-white b-2 transition-colors-300' },
     { 'home-link': 'text-6 uppercase bg-white display-inline-block p-1 my-2 rounded-3 b-black b-2 hover:bg-slate-3 text-center' },
+    { 'social-link': 'text-blue-7 hover:(text-red-5 no-underline)' },
+    [/btn-(big-)?(.*)/, ([, size, style]) => `bg-${style} rounded-4 ${size?.includes('big') ? 'text-lg px-5 py-4' : 'px-4 py-3'} border-2 border-${style}-2`],
   ],
   presets: [
     presetUno(),
