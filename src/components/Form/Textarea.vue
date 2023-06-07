7e458de2b2fd48fbcd77839f4a8176342eaaefdc
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-07 12:38:39 +0300

    styling textarea

--- src/components/Form/Textarea.vue
+++ src/components/Form/Textarea.vue
@@ -11,11 +11,6 @@ const modelValue = defineModel<string>()
 <template>
   <textarea
     v-model="modelValue"
-    class="my-20px h-40 py-2.5 px-5 my-1 w-full flex items-center gap-2 border-1
-      border-[#B2BDD4] rounded-lg text-white
-      shadow-none bg-transparent transition-box-shadow duration-300 focus:ring-transparent
-      focus-visible:outline-none focus:shadow-neutral-3/50
-      focus:border-neutral-4 focus:shadow-[0_0_0_0.2rem_var(--un-shadow-color)] resize-none
-      placeholder:leading-5.5" :maxlength="maxLength"
+    class="my-1 my-20px h-40 w-full flex resize-none items-center gap-2 border-1 border-[#B2BDD4] rounded-lg bg-transparent px-5 py-2.5 text-white shadow-none transition-box-shadow duration-300 focus:border-neutral-4 placeholder:leading-5.5 focus:shadow-[0_0_0_0.2rem_var(--un-shadow-color)] focus:shadow-neutral-3/50 focus-visible:outline-none focus:ring-transparent" :maxlength="maxLength"
   />
 </template>
