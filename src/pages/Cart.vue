51d4f101d423c408e1e39829c563fc19fce26a4f
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-08 15:21:58 +0300

    cart logo and page

---
+++ src/pages/Cart.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid mx-auto max-w-300 justify-center gap-6 p-4 lg:grid-cols-3 lt-lg:w-fit">
+    <div class="my2 b-1 b-black p2">
+      as
+    </div>
+    <div class="my2 b-1 b-black p2">
+      as
+    </div>
+    <div class="my2 b-1 b-black p2">
+      as
+    </div>
+    <div class="my2 b-1 b-black p2">
+      as
+    </div>
+    <div class="my2 b-1 b-black p2">
+      as
+    </div>
+    <div class="my2 b-1 b-black p2">
+      as
+    </div>
+  </div>
+</template>
