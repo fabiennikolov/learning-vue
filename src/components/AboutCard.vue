7ff3def756bf0b1c8c8f884cceec38f5b460bd62
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-09 15:30:01 +0300

    about card styling

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -12,9 +12,9 @@ defineProps<{
 
 <template>
   <div
-    b="2 gray"
+    b="2 transparent"
 
-    class="max-w-75 w-full overflow-hidden rounded-3 bg-slate-6 p-4 space-y-6 sm:p-8"
+    class="max-w-75 w-full overflow-hidden rounded-3 bg-slate-6 p-4 shadow-md drop-shadow-xl space-y-6 sm:p-8"
   >
     <h4 class="font-inter text-(center 2xl) font-extrabold leading-relaxed transition-all-300 hover:scale-110">
       {{ title }}
