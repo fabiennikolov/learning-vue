1e4c2120778be439597baea57afd29e5946b8439
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-09 15:30:08 +0300

    article card styling

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -12,7 +12,7 @@ defineProps<{
 
 <template>
   <div
-    b="1 black" class="max-w-75 w-full overflow-hidden b-black rounded-3 bg-slate-1 p-4 space-y-6 sm:p-8"
+    b="1 transparent" class="max-w-75 w-full overflow-hidden b-black rounded-3 bg-slate-1 p-4 shadow-xl drop-shadow-xl space-y-6 sm:p-8"
   >
     <h4 class="text-(center 2xl) font-(extrabold) leading-relaxed">
       {{ title }}
@@ -22,7 +22,7 @@ defineProps<{
         <img :src="image" class="mx-auto h-full object-contain">
       </div>
       <a
-        class="mx-auto inline-block b-1 b-black/70 rounded-2 bg-blue-3/60 px1 py1 text-sm font-medium transition-all-500 hover:scale-120"
+        class="mx-auto inline-block b-1 b-black/70 b-transparent rounded-2 bg-blue-1/90 px1 py1 text-sm font-medium shadow-md drop-shadow-md transition-all-500 hover:scale-120"
         :href="link.redirect" target="_blank"
       >
         {{ link.label }}</a>
