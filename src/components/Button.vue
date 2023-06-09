8ed9c2ff0b42c4fae84362b843a26b35c5aaedf3
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-09 13:55:17 +0300

    styling button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -13,7 +13,7 @@ withDefaults(defineProps<{
         ? 'bg-slate4 text-white hover:bg-slate-5'
         : 'bg-gray-7 text-white hover:bg-gray-5',
     ]"
-    class="b-1px rounded-2 px-3 py-2 text-center shadow-[0px_4px_4px_#323232] transition-colors-300"
+    class="b-transparent rounded-2 px-3 py-2 text-center shadow-[0px_4px_4px_#323232] shadow-md drop-shadow-sm transition-colors-300"
   >
     <slot />
   </button>
