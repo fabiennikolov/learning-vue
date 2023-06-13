25af0802dc54d96f70327c9632bb2f82a54109f1
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-13 16:58:00 +0300

    removing unused style from config

--- uno.config.ts
+++ uno.config.ts
@@ -7,8 +7,7 @@ import {
 export default defineConfig({
   shortcuts: [
     { 'form-link': 'rounded-3 py-1 px-2 hover:bg-slate-5 b-gray b-1 transition-colors-300' },
-    { 'home-link': 'text-sm transition-all-500 hover:scale-105 text-6 uppercase bg-white display-inline-block p-2 my-2 rounded-3 b-black b-2 hover:bg-slate-3 text-center b-transparent rounded-4 drop-shadow-md' },
-    { 'social-link': 'text-blue-7 hover:(text-red-5 no-underline)' },
+    { 'home-link': 'text-sm transition-all-500 hover:scale-105 text-6 uppercase bg-white display-inline-block p-1 my-2 b-black b-2 hover:bg-slate-3 text-center b-transparent rounded-2 drop-shadow-md' },
     [/btn-(big-)?(.*)/, ([, size, style]) => `bg-${style} rounded-4 ${size?.includes('big') ? 'text-lg px-5 py-4' : 'px-4 py-3'} border-2 border-${style}-2`],
   ],
   presets: [
