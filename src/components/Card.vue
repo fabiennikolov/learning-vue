aed9f83029fdf1897fda46f122e48a0932eae3f4
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-20 13:32:55 +0300

    implementing toast

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import type { ActiveToast } from 'vue-toast-notification'
+import { useToast } from 'vue-toast-notification'
 import type { Card } from '@/types'
+import 'vue-toast-notification/dist/theme-sugar.css'
 
 defineProps<Card>()
+
+const toastInstance = ref<ActiveToast>()
+
+const $toast = useToast()
+
+function showToast() {
+  toastInstance.value = $toast.success('Item added to cart!', {
+    position: 'top-right',
+  })
+}
 </script>
 
 <template>
@@ -14,10 +28,10 @@ defineProps<Card>()
       </p>
       <div class="flex flex-col items-center justify-center gap-2">
         <strong>BGN {{ price }}</strong>
-        <Button>
+        <Button @click="showToast">
           Add to Cart
         </Button>
-        <RouterLink v-if="to" :to="to" class="border-1 b-black/40 rounded-2 bg-slate2 px2 text-black/90 transition-all-200 hover:scale-105 hover:border-white hover:bg-gray hover:text-white">
+        <RouterLink v-if="to" instance :to="to" class="border-1 b-black/40 rounded-2 bg-slate2 px2 text-black/90 transition-all-200 hover:scale-105 hover:border-white hover:bg-gray hover:text-white">
           Show details
         </RouterLink>
       </div>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ToastPlugin from 'vue-toast-notification'
 import App from './App.vue'
 import router from './router'
+import 'vue-toast-notification/dist/theme-bootstrap.css'
 
 import '@unocss/reset/tailwind.css'
 import './assets/main.css'
@@ -11,7 +13,6 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(router)
-
+app.use(ToastPlugin)
 app.mount('#app')
-
 app.use(pinia)
