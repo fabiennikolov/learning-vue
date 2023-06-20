ef62bc06777e9237f09cdbc8f1c143bb60527685
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-20 13:40:14 +0300

    adding store

---
+++ src/stores/counter.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+
+export const useCubicCounter = defineStore('cubic-counter', () => {
+  const count = useStorage('cubuc-counter', 0)
+
+  const cubicCount = computed(() => count.value ** 3)
+
+  function increment() {
+    count.value++
+  }
+  function decrement() {
+    count.value--
+  }
+
+  return { count, cubicCount, increment, decrement }
+})
