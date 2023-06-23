842c646cd97ecaee0c898fcdec4255175eb60adf
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-23 18:44:22 +0300

    css for auth layout

--- src/layouts/auth.vue
+++ src/layouts/auth.vue
@@ -1,6 +1,5 @@
 <template>
-  <div bg-red h-100vh>
-    swfffewfwefew
+  <div class="min-h-screen flex flex-col justify-between flex flex-col items-center bg-blue2">
     <RouterView />
   </div>
 </template>
