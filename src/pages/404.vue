f8f5236a3dfedc3e4268583c56c0d5d798aeed66
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-23 15:30:17 +0300

    set up 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h100% b-1 b-black bg-#8abcbc text-center">
+    <div class="display-inline-block h70 text-220px tracking-3">
+      <h1>404</h1>
+      <hr class="h4 w100 b-t-5 p0 text-center">
+    </div>
+    <div class="text-center text-4em tracking-3 uppercase">
+      <p>the page</p>
+    </div>
+    <div class="mt4 w100% text-center text-11 leading-1.2em uppercase">
+      <p>was not found</p>
+    </div>
+    <RouterLink
+      to="/" class="home-link"
+    >
+      Go to homepage
+    </RouterLink>
+  </div>
+</template>
