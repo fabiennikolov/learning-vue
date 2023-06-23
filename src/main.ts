11e9316690d2bf6d500ac987df01c6ad3c9c86ac
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-23 15:29:22 +0300

    Importing Vite SSG

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ToastPlugin from 'vue-toast-notification'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
-import router from './router'
+import routes from './router'
 import 'vue-toast-notification/dist/theme-bootstrap.css'
 
 import '@unocss/reset/tailwind.css'
 import './assets/main.css'
 import 'virtual:uno.css'
 
-const app = createApp(App)
-const pinia = createPinia()
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ app, router, routes, isClient, initialState }) => {
+    // const head = createHead()
+    const pinia = createPinia()
 
-app.use(router)
-app.use(ToastPlugin)
-app.mount('#app')
-app.use(pinia)
+    if (isClient)
+      pinia.state.value = (initialState.pinia) || {}
+
+    else
+      initialState.pinia = pinia.state.value
+
+    app
+      .use(pinia)
+      .use(ToastPlugin)
+  },
+)
