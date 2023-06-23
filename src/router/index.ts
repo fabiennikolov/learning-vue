4e8e1572fa7f44805d9e9cd86a17ab0617665b50
Fabien Nikolov <fabiennikolov9@gmail.com>
2023-06-23 15:29:34 +0300

    exporting default routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,3 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-
-  // routes,
-  // [...routes,
-  //   { path: '/404', component: NotFound },
-  //   { path: '*', redirect: '/404' }
-  // ],
-})
-
-router.addRoute({ path: '/404', name: 'NotFound', component: Error })
-router.addRoute({ path: '/:pathMatch(.*)*', redirect: '/404' })
-
-export default router
+export default routes
